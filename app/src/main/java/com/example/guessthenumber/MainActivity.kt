85384a9bcc14f0489315685c0a13c5d7ddff729b
batikanoz123 <batikanoz123@gmail.com>
2025-03-19 15:23:30 +0000

    Creating user input (Also fixed line endings issue)

--- app/src/main/java/com/example/guessthenumber/MainActivity.kt
+++ app/src/main/java/com/example/guessthenumber/MainActivity.kt
@@ -1,47 +1,28 @@
 package com.example.guessthenumber
 
+import android.app.Activity
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.guessthenumber.ui.theme.GuessTheNumberTheme
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
-class MainActivity : ComponentActivity() {
+class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            GuessTheNumberTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+        setContentView(R.layout.activity_main)
+
+        val userInput = findViewById<EditText>(R.id.userInput)
+        val submitButton = findViewById<Button>(R.id.submitButton)
+        val resultText = findViewById<TextView>(R.id.resultText)
+
+        submitButton.setOnClickListener {
+            val userNumber = userInput.text.toString().toIntOrNull()
+
+            if (userNumber != null) {
+                resultText.text = "You entered: $userNumber"
+            } else {
+                resultText.text = "Please enter a valid number!"
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    GuessTheNumberTheme {
-        Greeting("Android")
     }
 }
